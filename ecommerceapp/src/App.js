fe196ebf7e7d248a7b891ada974f96c9f3e5ae2c
Abel <josephine.abel@outlook.fr>
2022-09-24 18:56:00 +0200

    add styled-components for the app

--- ecommerceapp/src/App.js
+++ ecommerceapp/src/App.js
@@ -2,17 +2,33 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Registration from './pages/Registration';
 import {Routes, Route, Link} from "react-router-dom"
+import styled, {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*, ::before, ::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body, html {
+  height: 100vh;
+  width: 100vw;
+}
+
+`
+
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Routes>
 
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<Registration/>}/>
 
 
-      </Routes>
+      </Routes> */}
 
     </div>
   );
